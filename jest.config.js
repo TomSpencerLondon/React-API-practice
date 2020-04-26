de80bba3f017b8440b812bc944f63e3826053e71
Tom <thomas.spencer@codurance.com>
2020-04-26 23:55:12 +0100

    change back

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,5 @@ require('@babel/polyfill')
 module.exports = {
   preset: 'jest-puppeteer',
   testRegex: './*\\.spec\\.js$',
-  setupTestFrameworkScriptFile: 'expect-puppeteer'
+  setupFilesAfterEnv: ['expect-puppeteer']
 }
