e4c5a7b6931a4857d03def4f09ff726fb50ade92
Tom <thomas.spencer@codurance.com>
2020-04-26 23:46:46 +0100

    delete cruft from setupFilesAfterEnv

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,5 @@ require('@babel/polyfill')
 module.exports = {
   preset: 'jest-puppeteer',
   testRegex: './*\\.spec\\.js$',
-  setupFilesAfterEnv: ['expect-puppeteer', "<rootDir>/node_modules/regenerator-runtime/runtime"]
+  setupFilesAfterEnv: ['expect-puppeteer']
 }
